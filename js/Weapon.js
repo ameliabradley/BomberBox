f2d275d6020d9346103c9851fcb3668bb7d34642
Lee Bradley <lbradley@cyberitas.com>
2013-06-23 23:11:38 -0600

    Added copyright notice

--- js/Weapon.js
+++ js/Weapon.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright © 2012-2013 Lee Bradley 
+ * All Rights Reserved
+ * 
+ * NOTICE: All information herein is, and remains the property of
+ * Lee Bradley. Dissemation of this information or reproduction of
+ * this material is strictly forbidden unless prior written permission
+ * is obtained from Lee Bradley.
+ * 
+ * The above copyright notice and this notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
 Weapon = function() {
    var self = this,
       m_id,
